093a045c97776bae661bf574e39f977805a61960
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-06-13 17:39:13 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('chapterselect').addEventListener('change', function() {
     if (this.value) {
         window.location.href = this.value;
+        this.selectedIndex = 0;
     }
 });
